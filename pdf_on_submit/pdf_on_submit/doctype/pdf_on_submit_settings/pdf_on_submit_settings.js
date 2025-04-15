2ecdd95d087b387f5d522fc3d8891a60bfd5c55f
Raffael Meyer <14891507+barredterra@users.noreply.github.com>
2025-04-15 11:32:49 +0200

    fix(PDF on Submit Settings): UX improvements (#64)

--- pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.js
+++ pdf_on_submit/pdf_on_submit/doctype/pdf_on_submit_settings/pdf_on_submit_settings.js
@@ -10,14 +10,25 @@ frappe.ui.form.on("PDF on Submit Settings", {
 				},
 			};
 		});
+
+		frm.set_query("document_type", "enabled_for", function (doc, cdt, cdn) {
+			return {
+				filters: {
+					is_submittable: 1,
+				},
+			};
+		});
 	},
-	enabled_for_on_form_rendered(frm, dt, a, b, c) {
+	enabled_for_on_form_rendered(frm) {
 		const row = frm.cur_grid.doc;
-		const filters = row.filters ? JSON.parse(row.filters) : [];
+		const parent = frm.cur_grid.wrapper.find("[data-fieldname='filter_area']");
+		parent.empty();
+
+		const filters = row.filters && row.filters !== "[]" ? JSON.parse(row.filters) : [];
 
 		frappe.model.with_doctype(row.document_type, () => {
 			const filter_group = new frappe.ui.FilterGroup({
-				parent: frm.cur_grid.wrapper.find("[data-fieldname='filter_area']"),
+				parent: parent,
 				doctype: row.document_type,
 				on_change: () => {
 					frappe.model.set_value(
@@ -31,5 +42,26 @@ frappe.ui.form.on("PDF on Submit Settings", {
 
 			filter_group.add_filters_to_filter_group(filters);
 		});
+	},
+});
+
+frappe.ui.form.on("Enabled DocType", {
+	document_type(frm, cdt, cdn) {
+		const row = locals[cdt][cdn];
+		frappe.model.set_value(row.doctype, row.name, "filters", "[]");
+
+		if (row.print_format) {
+			// Check if the print format is valid for the document type
+			// If not, set the print format to an empty string
+			frappe.db.get_value("Print Format", row.print_format, "doc_type").then((r) => {
+				if (r.message.doc_type !== row.document_type) {
+					frappe.model.set_value(row.doctype, row.name, "print_format", "");
+				}
+			});
+		}
+
+		if (frm.cur_grid) {
+			frm.events.enabled_for_on_form_rendered(frm);
+		}
 	},
 });
